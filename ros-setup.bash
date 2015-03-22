024ed50435b0fdb8d7baeb02fbcc2cb73409410d
Salah-Eddine Missri <missrisalaheddine@gmail.com>
2015-03-22 14:03:16 +0100

    Use aptitude instead of apt-get & override OS detection

--- ros-setup.bash
+++ ros-setup.bash
@@ -4,8 +4,12 @@
 echo "Installing ROS..."
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu trusty main" > /etc/apt/sources.list.d/ros-latest.list'
 wget https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -O - | sudo apt-key add -
-sudo apt-get update
-sudo apt-get install -y ros-indigo-desktop-full
+sudo aptitude update
+sudo aptitude install -y ros-indigo-desktop-full
+
+# If using elementary OS uncomment the following two lines or if using another distro, replace according to this structure: export ROS_OS_OVERRIDE="DISTRIB_ID:DISTRIB_RELEASE:DISTRIB_CODENAME"
+# export ROS_OS_OVERRIDE="elementary OS:0.3:freya" >> ~/.bashrc
+# bash
 
 # Setup ROS
 echo "Setting up ROS..."
@@ -13,21 +17,22 @@ sudo rosdep init
 rosdep update
 echo "source /opt/ros/indigo/setup.bash" >> ~/.bashrc
 source ~/.bashrc
-sudo apt-get install -y python-rosinstall
+sudo aptitude install -y python-rosinstall
 
 # Install missing ROS packages
 echo "Installing some missing ROS packages..."
-sudo apt-get install -y ros-indigo-ros-control ros-indigo-ros-controllers ros-indigo-joystick-drivers ros-indigo-robot-state-publisher
+sudo aptitude install -y ros-indigo-ros-control ros-indigo-ros-controllers ros-indigo-joystick-drivers ros-indigo-robot-state-publisher
 
 # Install Gazebo
 echo "Installing and setting up Gazebo..."
-sudo sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-latest.list'
+sudo sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu trusty main" > /etc/apt/sources.list.d/gazebo-latest.list'
 wget http://packages.osrfoundation.org/gazebo.key -O - | sudo apt-key add -
-sudo apt-get update
-sudo apt-get install -y ros-indigo-gazebo-ros-pkgs ros-indigo-gazebo-ros-control gazebo2 libsdformat1 ros-indigo-gazebo-plugins ros-indigo-gazebo-ros
+sudo aptitude update
+sudo aptitude install -y ros-indigo-gazebo-ros-pkgs ros-indigo-gazebo-ros-control gazebo2 libsdformat1 ros-indigo-gazebo-plugins ros-indigo-gazebo-ros
 
 # Setup ROS dependencies
 echo "Setting up ROS dependencies..."
+bash
 rosdep install robot_state_publisher urdf xacro controller_manager geometry_msgs joy ros_control ros_controllers sensor_msgs std_msgs gazebo_msgs gazebo_plugins gazebo_ros gazebo_ros_control joy
 
 echo "Finishing... Done."
