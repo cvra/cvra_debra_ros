3f285bf9f41dbc227c693705f276fc3267aaf8e4
Salah-Eddine Missri <missrisalaheddine@gmail.com>
2015-05-19 12:57:27 +0200

    Complete feedback stream callbacks/publishers

--- master_bridge/scripts/feedback_stream.py
+++ master_bridge/scripts/feedback_stream.py
@@ -17,6 +17,15 @@ def position_pid_cb(args):
 def index_cb(args):
     joint_publisher(args, 'index')
 
+def motor_encoder_cb(args):
+    joint_publisher(args, 'motor_encoder')
+
+def motor_position_cb(args):
+    joint_publisher(args, 'motor_position')
+
+def motor_torque_cb(args):
+    joint_publisher(args, 'motor_torque')
+
 def joint_publisher(args, type):
     global pub_joint, joint
 
@@ -49,6 +58,26 @@ def joint_publisher(args, type):
         joint.index = index
         joint.position.measured = index
 
+    elif type == 'motor_encoder':
+        node_id, raw_encoder = tuple(args)
+
+        joint.node_id = node_id
+        joint.raw_encoder_value = raw_encoder
+
+    elif type == 'motor_position':
+        node_id, position, velocity = tuple(args)
+
+        joint.node_id = node_id
+        joint.position.measured = position
+        joint.velocity.measured = velocity
+
+    elif type == 'motor_torque':
+        node_id, torque, position = tuple(args)
+
+        joint.node_id = node_id
+        joint.torque = position
+        joint.position.measured = position
+
     pub_joint.publish(joint)
 
 if __name__ == '__main__':
@@ -61,7 +90,10 @@ if __name__ == '__main__':
     callbacks = {'current_pid': current_pid_cb, \
                  'velocity_pid': velocity_pid_cb, \
                  'position_pid': position_pid_cb, \
-                 'index': index_cb}
+                 'index': index_cb, \
+                 'motor_encoder': motor_encoder_cb, \
+                 'motor_position': motor_position_cb, \
+                 'motor_torque': motor_torque_cb}
 
     RequestHandler = create_request_handler(callbacks)
     server = socketserver.UDPServer(TARGET, RequestHandler)
